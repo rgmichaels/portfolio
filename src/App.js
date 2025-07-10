6761cbd1df3496af6ecaca6576ced1051416bd9b
rgmichaels <95025342+rgmichaels@users.noreply.github.com>
2025-07-10 08:50:40 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ function App() {
         <h2>Featured Project</h2>
         <p><strong>PWFramework</strong>: A GitHub Actions-based test automation demo</p>
         <a href="https://github.com/rgmichaels/PWFramework">View on GitHub</a>
+        <p><strong>Playwright test results</strong>: Test results for this page</p>
+        <a href="https://github.com/rgmichaels/PWFramework/actions/runs/15028271564/artifacts/3125327398">Download Test results</a>
       </section>
     </div>
   );
